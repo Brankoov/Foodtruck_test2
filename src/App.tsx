23958f49b7568a5ab4862b5fcc0cce1874607244
Branko <banne_nikolic@hotmail.com>
2025-03-06 17:00:00 +0100

    Add: Desert menu, some styling around the title h1.

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,19 @@ function App() {
     <>
       
       <h1>Foodtruck test</h1>
+      <hr className="divider" />
       
       <main>
         <section className='MainMenu'>
           <h2>Main Menu</h2>
           <article className='MainOrders'> Big burger 90kr</article>
           <article className='MainOrders'> Chicken wrap 70kr</article>
+        </section>
+
+        <section className='Deserts'>
+          <h2>Deserts</h2>
+          <article className='DesertOrders'>Cheesecake 40kr</article>
+          <article className='DesertOrders'>Strawberry cake 40kr</article>
         </section>
           
       </main>

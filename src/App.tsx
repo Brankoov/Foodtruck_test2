cdc52622ba33bd6ffca6bd94f082377f241ff91d
Branko <banne_nikolic@hotmail.com>
2025-03-06 15:58:13 +0100

    Add: Did some styling in App.css and index.css. Also added a Main menu with 2 orders in it

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,16 @@ function App() {
     <>
       
       <h1>Foodtruck test</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <main>
+        <section className='MainMenu'>
+          <h2>Main Menu</h2>
+          <article className='MainOrders'> Big burger 90kr</article>
+          <article className='MainOrders'> Chicken wrap 70kr</article>
+        </section>
+          
+      </main>
+      
     </>
   )
 }
